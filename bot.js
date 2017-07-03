f1340fba533880a1f0c599eccafa8821d82bd6b7
ZachySwag <zman000@icloud.com>
2017-07-03 14:06:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ bot.on("ready", function() {
   console.log("I am ready");
 });
 
-bot.on("guildMemberAdd", function(message) {
+bot.on("guildMemberAdd", function(member) {
   member.build.channels.find("name", "general-chat").sendMessage(member.toString() + " Welcome to the NintappleYT Official Discord! You now have the role of Member, and you have basic permissions.");
 
   member.addRole(member.guild.roles.find("name", "(Members) The Freshest Squids"));
