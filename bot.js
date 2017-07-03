aebbd59776e19b98625b2851ce63587e60be2c42
ZachySwag <zman000@icloud.com>
2017-07-03 13:01:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,7 @@ bot.on ("message", function(message) {
       var embed = new Discord.RichEmbed()
         .addField("$hello:", "Say hi to me!")
         .addField("$info:", "Find out who I am!");
+        .setColor(red)
       message.channel.sendEmbed(embed);
       break;
     default:
